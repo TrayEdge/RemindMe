478fa4f2d3451e4297a31dd1d52668c356e74eeb
Michael <undermyclouds@gmail.com>
2015-09-02 20:05:56 +0300

    ToolBar implementation

--- app/src/main/java/com/example/cloud/remindme/MainActivity.java
+++ app/src/main/java/com/example/cloud/remindme/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.cloud.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Cloud on 02.09.2015.
  */
 public class MainActivity extends Activity {
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRoolBar();
     }
+
+    private void initRoolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
 }
