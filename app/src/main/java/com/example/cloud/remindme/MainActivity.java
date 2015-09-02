5b3afe870496d1fa664ad0a379d0f9abb1a475aa
Michael <undermyclouds@gmail.com>
2015-09-02 08:41:29 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/com/example/cloud/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cloud.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Cloud on 02.09.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
